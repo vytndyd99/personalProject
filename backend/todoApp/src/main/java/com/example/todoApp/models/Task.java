e6b2911f11f7b29d90c3dfa49dec779f39a07cc3
pyosuyong <vytndyd99@naver.com>
2023-12-18 21:35:30 +0900

    23.12,18

---
+++ backend/todoApp/src/main/java/com/example/todoApp/models/Task.java
@@ -0,0 +1,21 @@
+package com.example.todoApp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private boolean done;
+}
